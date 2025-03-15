a141b60978fc06752e7596498366fb8ed6d6359f
Darmin T. <61888777+CoderTea-1@users.noreply.github.com>
2025-03-15 04:11:19 +0000

    Test 2

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -93,7 +93,6 @@ export default function Instructions({
 
   <div className="row center">
     <h5>
-      <p className="container xl:hidden">Click "Open the Keyboard" and</p>
       <p>
         Press the{" "}
         <a className="btn disabled">
